533ec8fda18f9a7f2a7c5c3b65369981621eeb22
Justin Farnsworth <fjj12697@gmail.com>
2024-08-26 10:14:35 -0400

    install binarized version of psycopg

--- api/api/settings.py
+++ api/api/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'HOST': os.getenv('DB_HOST'),
         'USER': os.getenv('DB_USER'),
